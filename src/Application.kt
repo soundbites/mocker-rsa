978370135a72f96a2bdc1b11163b8d3309521fbf
jamiecraane <jcraane@gmail.com>
2020-01-06 07:34:37 +0100

    Add simple prototype for replacing tokens in responses

--- src/Application.kt
+++ src/Application.kt
@@ -1,16 +1,22 @@
-import io.ktor.application.Application
-import io.ktor.application.ApplicationCallPipeline
-import io.ktor.application.install
+import io.ktor.application.*
 import io.ktor.features.CORS
+import io.ktor.features.Compression
 import io.ktor.features.DefaultHeaders
 import io.ktor.http.HttpHeaders
 import io.ktor.http.HttpMethod
+import io.ktor.http.content.OutgoingContent
+import io.ktor.http.content.TextContent
 import io.ktor.http.content.resources
 import io.ktor.http.content.static
+import io.ktor.request.receiveText
+import io.ktor.response.ApplicationSendPipeline
+import io.ktor.response.respondText
 import io.ktor.routing.routing
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
-import nl.capaxit.mock
+import io.ktor.util.AttributeKey
+import io.ktor.util.pipeline.PipelineContext
+import io.ktor.util.pipeline.PipelinePhase
 
 fun main(args: Array<String>) {
     //io.ktor.server.netty.main(args) // Manually using Netty's EngineMain
@@ -41,6 +47,9 @@ fun Application.module() {
         header("X-Engine", "Ktor") // will send this header with each response
     }
 
+//    todo add config replace values.
+    install(TokenReplaceFeature())
+
 //    Use interceptors for global logic.
     intercept(ApplicationCallPipeline.Call) {
 //        delay(1500)
@@ -54,5 +63,28 @@ fun Application.module() {
 
     mock(basePath = "api/v1") {
         persons()
+    }
+}
+
+class TokenReplaceFeature : ApplicationFeature<ApplicationCallPipeline, Compression.Configuration, TokenReplacer> {
+    override val key = AttributeKey<TokenReplacer>("replacer")
+
+    override fun install(pipeline: ApplicationCallPipeline, configure: Compression.Configuration.() -> Unit): TokenReplacer {
+        val feature = TokenReplacer()
+        pipeline.sendPipeline.intercept(ApplicationSendPipeline.ContentEncoding) {
+            feature.interceptor(this)
+        }
+        return feature
+    }
+}
+
+class TokenReplacer {
+    suspend fun interceptor(context: PipelineContext<Any, ApplicationCall>) {
+        val call = context.call
+        val message = context.subject
+        if (message is TextContent) {
+            val replaced = message.text.replace("{HOST_IP}", "10.0.0.2")
+            context.proceedWith(TextContent(replaced, message.contentType, call.response.status()))
+        }
     }
 }
--- src/mocks/Persons.kt
+++ src/mocks/Persons.kt
@@ -1,12 +1,9 @@
 import io.ktor.application.call
 import io.ktor.routing.Route
 import io.ktor.routing.get
-import kotlinx.coroutines.delay
-import nl.capaxit.respondContents
 
 fun Route.persons() {
     get("persons") {
-//        delay(1000)
         call.respondContents("/responses/persons.json")
     }
 }
