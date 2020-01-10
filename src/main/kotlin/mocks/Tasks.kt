8058486fbff1877cdd23a47b3dd1c9b6b819fcbe
jamiecraane <jcraane@gmail.com>
2020-01-10 15:30:07 +0100

    Add tasks resources for testing

---
+++ src/main/kotlin/mocks/Tasks.kt
@@ -0,0 +1,24 @@
+import io.ktor.application.call
+import io.ktor.http.HttpStatusCode
+import io.ktor.response.respond
+import io.ktor.response.respondText
+import io.ktor.routing.*
+import nl.jcraane.mocker.respondContents
+
+fun Route.tasks() {
+    get("tasks") {
+        call.respondContents("/responses/tasks.json")
+    }
+
+    post("tasks") {
+        call.respond(HttpStatusCode.Created)
+    }
+
+    put("tasks") {
+        call.respond(HttpStatusCode.Created)
+    }
+
+    delete("tasks") {
+        call.respond(HttpStatusCode.NoContent)
+    }
+}
