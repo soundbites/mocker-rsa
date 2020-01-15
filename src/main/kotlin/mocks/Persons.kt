e92283b063d071607045be281344fd00813d56ea
jamiecraane <jcraane@gmail.com>
2020-01-15 21:09:00 +0100

    Renabled mocks

--- src/main/kotlin/mocks/Persons.kt
+++ src/main/kotlin/mocks/Persons.kt
@@ -1,20 +1,23 @@
 
-import io.ktor.routing.Route
+import io.ktor.application.call
+import io.ktor.http.HttpStatusCode
+import io.ktor.response.respond
+import io.ktor.routing.*
+import nl.jcraane.mocker.respondContents
 
 fun Route.persons() {
-    /*get("persons") {
+    get("persons") {
         call.respondContents("/responses/persons.json")
-    }*/
+    }
 
-    /*post("persons") {
+    post("persons") {
         call.respond(HttpStatusCode.Created)
-    }*/
-/*
+    }
     put("persons") {
         call.respond(HttpStatusCode.Created)
     }
 
     delete("persons") {
         call.respond(HttpStatusCode.NoContent)
-    }*/
+    }
 }
