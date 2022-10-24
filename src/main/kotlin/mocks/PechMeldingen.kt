2a9fa37ceefc14484fa01e41735debeefcb07c9b
Robert van Loghem <robert.van.loghem@xs4all.nl>
2022-10-24 10:51:50 +0200

    Added additional route

--- src/main/kotlin/mocks/PechMeldingen.kt
+++ src/main/kotlin/mocks/PechMeldingen.kt
@@ -5,6 +5,7 @@ import io.ktor.http.*
 import io.ktor.request.*
 import io.ktor.response.*
 import io.ktor.routing.*
+import io.ktor.util.pipeline.*
 import kotlinx.serialization.ImplicitReflectionSerializer
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.stringify
@@ -17,25 +18,31 @@ val meldingen = mutableListOf<IntakeStates>()
 @UseExperimental(ImplicitReflectionSerializer::class)
 fun Route.PechMeldingen() {
     get("disinfo/v1/pechMeldingen/relnr/{relnr}") {
+        handlePechmeldingenCall()
+    }
+    get("disinfo/v1/pechmeldingen/relnr/{relnr}") {
+        handlePechmeldingenCall()
+    }
+}
 
-        val state = meldingen.firstOrNull() {
-            it.report.relation.relationNumber == call.parameters.get("relnr")
-        }
+private suspend fun PipelineContext<Unit, ApplicationCall>.handlePechmeldingenCall() {
+    val state = meldingen.firstOrNull() {
+        it.report.relation.relationNumber == call.parameters.get("relnr")
+    }
 
-        val next = state?.nextStatus()
-        val evolved = state?.evolve()
+    val next = state?.nextStatus()
+    val evolved = state?.evolve()
 
-        if (state != null) {
-            meldingen.remove(state)
-        }
-        if (evolved != null) {
-            meldingen.add(evolved)
-        }
+    if (state != null) {
+        meldingen.remove(state)
+    }
+    if (evolved != null) {
+        meldingen.add(evolved)
+    }
 
-        if (next != null) {
-            call.respondText(Json.stringify(Status.serializer(), next), ContentType.Application.Json)
-        } else  {
-            call.respondContents("/responses/roadside-assistance-status-not-found.json", ContentType.Application.Json)
-        }
+    if (next != null) {
+        call.respondText(Json.stringify(Status.serializer(), next), ContentType.Application.Json)
+    } else {
+        call.respondContents("/responses/roadside-assistance-status-not-found.json", ContentType.Application.Json)
     }
 }
